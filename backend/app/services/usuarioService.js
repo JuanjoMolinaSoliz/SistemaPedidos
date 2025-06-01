4d8693cab5659a636909606ac05d0f816101a5c2
JuanjoMolinaSoliz <juanjose.m.s@hotmail.com>
2025-06-01 14:17:18 -0400

    añado actualizar ultima conexion

--- backend/app/services/usuarioService.js
+++ backend/app/services/usuarioService.js
@@ -9,6 +9,10 @@ class UsuarioService {
     async crearUsuario(usuario, password){
         return await this.usuarioRepository.crearUsuario(usuario, password);
     }
+    async actualizarUltimaConexion(usuarioId) {
+        console.log(`Simulando actualización de la última conexión para el usuario con ID: ${usuarioId}`);
+        return { success: true, message: `Última conexión actualizada para el usuario ${usuarioId}` };
+    }
 }
 
 module.exports = UsuarioService;
